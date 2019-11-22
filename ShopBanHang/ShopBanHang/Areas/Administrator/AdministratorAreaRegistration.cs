1fa421f7c01167a852a7aa90cf255a2230094a80
lamthienlap123 <54541801+lamthienlap123@users.noreply.github.com>
2019-11-22 23:03:45 +0700

    web

---
+++ ShopBanHang/ShopBanHang/Areas/Administrator/AdministratorAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace ShopBanHang.Areas.Administrator
+{
+    public class AdministratorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Administrator";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Administrator_default",
+                "Administrator/{controller}/{action}/{id}",
+                new { controller="Home", action = "Index", id = UrlParameter.Optional },
+                new[] { "ShopBanHang.Areas.Administrator.Controllers" }
+            );
+        }
+    }
+}
---
+++ ShopBanHang/ShopBanHang/Areas/Administrator/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopBanHang.Areas.Administrator.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.AdminContext db = new Models.AdminContext();
+        
+        // GET: Administrator/Home
+        public ActionResult Index()
+        {
+            if (Session["accname"] == null)
+            {
+                Session["accname"] = null;
+                return RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
